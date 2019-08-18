b825525a4787dd5e4c97da37f445424acae68a40
Tammy Metz <tammy@tammymetz.com>
2019-08-18 03:45:30 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0;
+
 
 function parseCode(e) {
+  let index = 0;
   const key = parseInt(e.detail || e.which);
   if (key === code[index]) {
     index++;
