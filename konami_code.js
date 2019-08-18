cbd135732423a2993eecd2a4c095fd798c752641
Tammy Metz <tammy@tammymetz.com>
2019-08-18 03:47:38 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,7 @@ function init() {
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
       index++;
-      if (index === alphabet.length) {
+      if (index === code.length) {
         alert("Hurray!");
         index = 0;
       }
