3efb690c320f646114b7a4ec8434631f92c931be
Tammy Metz <tammy@tammymetz.com>
2019-08-18 03:47:14 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,24 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-
-function parseCode(e) {
-  let index = 0;
-  const key = parseInt(e.detail || e.which);
-  if (key === code[index]) {
-    index++;
-    if (index === alphabet.length) {
-      alert("Hurray!");
-      index = 0;
-    }
-  }
-  else {
-    index = 0;
-  }
-}
-
 function init() {
   // Write your JavaScript code inside the init() function
-  document.body.addEventListener('keydown', parseCode);
+  document.body.addEventListener('keydown', function(e) {
+    let index = 0;
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === alphabet.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    }
+    else {
+      index = 0;
+    }
+  });
 
 }
 
